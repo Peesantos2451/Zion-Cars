7954f4727bf334f70c74b39731c1d56442360e42
Dev.Pedro <pedro.gsantos2451@gmail.com>
2023-05-03 14:59:46 -0300

    First Commit

---
+++ Components/login.js
@@ -0,0 +1,12 @@
+function login () {
+  //Get the values entered by the user
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+}
+
+  //Check if the username and password are valid
+  if (username == "exampleuser" && password == "examplepass") {
+      alert("Login successful!");
+  } else {
+    alert("Invalid username or password!")
+  }
